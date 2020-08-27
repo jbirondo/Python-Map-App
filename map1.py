03d5684e0e8810d907f339c6acb0bef5892398cd
Eugene Birondo <jbirondo@gmail.com>
2020-08-26 18:46:01 -0700

    Adding markers

--- map1.py
+++ map1.py
@@ -1,8 +1,8 @@
 import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-
-
-map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi, I'm a marker", icon=folium.Icon(color="green")))
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi, I'm a marker", icon=folium.Icon(color="green")))
+map.add_child(fg)
 
 map.save("Map1.html")
