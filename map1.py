ddbc5d426d9a09be1442a96fa24e0ff963b68f1f
Eugene Birondo <jbirondo@gmail.com>
2020-08-27 01:28:17 -0700

    Adding Choropleth

--- map1.py
+++ map1.py
@@ -34,7 +34,8 @@ fg.add_child(folium.GeoJson(data=open(
     encoding="utf-8-sig").read(),
     style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
                               else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 
-                              else 'red'}))
+                              else 'red'}
+    ))
 
 
 
